e3cbd731f67d913eaa8e8267e2b1a55c86f79e22
AashishRautela <asisrautela612@gmail.com>
2025-04-03 00:16:04 +0530

    Add axios dependency and implement flight availability check in booking service

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -1,10 +1,25 @@
 const { StatusCodes } = require('http-status-codes');
 const { BookingService } = require('../services');
 const { ErrorResponse, SuccessResponse } = require('../utils/common');
+const AppError = require('../utils/errors/appError');
 
 const createBooking = async (req, res) => {
   try {
-    const booking = await BookingService.createBooking(req.body);
+    const { flightId, userId, noOfSeats } = req.body;
+
+    if (!flightId || !userId || !noOfSeats) {
+      ErrorResponse.message = 'Something went wrong while creating booking ';
+      ErrorResponse.error = new AppError(
+        ['Request data missing'],
+        StatusCodes.BAD_REQUEST
+      );
+      return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    }
+    const booking = await BookingService.createBooking({
+      flightId,
+      userId,
+      noOfSeats
+    });
     SuccessResponse.data = booking;
     return res.status(StatusCodes.OK).json(SuccessResponse);
   } catch (error) {
--- src/services/booking.service.js
+++ src/services/booking.service.js
@@ -1,13 +1,42 @@
 const { StatusCodes } = require('http-status-codes');
 const { BookingRepository } = require('../repositories');
 const AppError = require('../utils/errors/appError');
-const { Booking } = require('../models/index');
+const db = require('../models');
+const { default: axios } = require('axios');
+const { ServerConfig } = require('../config');
 
 const createBooking = async (data) => {
   try {
-    const booking = await BookingRepository.create(data);
+    const flightService = ServerConfig.FLIGHT_SERVICE;
+
+    const booking = await db.sequelize.transaction(async (t) => {
+      const flight = await axios.get(
+        `${flightService}/api/v1/flight/${data.flightId}`
+      );
+
+      // first check if flight is present or not
+      if (!flight || !flight.data || !flight.data.data) {
+        throw new AppError(['Flight not found'], StatusCodes.BAD_REQUEST);
+      }
+
+      const flightData = flight?.data?.data;
+      // check if seats are available in flight or not
+      if (flightData?.totalSeats < data?.noOfSeats) {
+        const error = new AppError(
+          ['Not enough seats remaining'],
+          StatusCodes.BAD_REQUEST
+        );
+        throw error;
+      }
+
+      return flight.data.data;
+    });
+
     return booking;
   } catch (error) {
+    if (error instanceof AppError) {
+      throw error;
+    }
     console.log('error', error);
     throw new AppError(
       ['Something went wrong while creating booking'],
